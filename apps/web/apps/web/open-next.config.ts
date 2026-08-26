da9765a97170f8f97c8bf2d1df9b96621655aa5d
Anant Sharma <asindia23@gmail.com>
2026-08-26 09:21:15 +0000

    create wrangler.json and open-next.config.ts in apps/web and changed the build in package.json to opennextjs-cloudflare from npm build

---
+++ apps/web/apps/web/open-next.config.ts
@@ -0,0 +1,10 @@
+// apps/web/open-next.config.ts
+const config = {
+  default: {
+    deployment: {
+      name: "zeroth", // This must match your Cloudflare Worker name
+    },
+  },
+};
+
+export default config;
